3715fd0ac56cf87987f8219170bdbb20554a63db
Mariano Fuentes <marfuen98@gmail.com>
2023-05-11 17:00:28 -0400

    add dropdown to logout

--- src/components/Signin/index.tsx
+++ src/components/Signin/index.tsx
@@ -1,7 +1,8 @@
 import { PAPER_CLIENT_ID } from '@/lib/environment-variables';
-import { Button } from '@chakra-ui/react';
+import { Button, Menu, MenuButton, MenuItem, MenuList, Portal } from '@chakra-ui/react';
 import { useAddress, useConnectionStatus, useDisconnect, useLogin, useLogout, usePaperWallet, usePaperWalletUserEmail, useUser } from '@thirdweb-dev/react';
 import { useCallback, useEffect, useRef } from 'react';
+import { FiArrowDown, FiChevronDown, FiChevronUp } from 'react-icons/fi';
 
 export const Signin = ({ ...rest }) => {
   const loginAttempted = useRef(false);
@@ -44,20 +45,59 @@ export const Signin = ({ ...rest }) => {
   }, [address, connect, disconnect, logout]);
 
   return (
-    <Button
-      isLoading={connecting}
-      onClick={handleLoginOrLogout}
-      minW="85px"
-      bg="white"
-      color="black"
-      _hover={{
-        bg: "white",
-        color: "black",
-        opacity: 0.8,
-      }}
-      {...rest}
-    >
-      {(address || isLoggedIn && email) ? email : 'Sign in'}
-    </Button>
+    <>
+    {!address && !isLoggedIn && !isLoading ? (
+      <Button
+        as={Button}
+        isLoading={connecting}
+        onClick={handleLoginOrLogout}
+        rightIcon={address ? <FiArrowDown /> : undefined}
+        minW="85px"
+        bg="white"
+        color="black"
+        _hover={{
+          bg: "white",
+          color: "black",
+          opacity: 0.9,
+        }}
+        {...rest}
+      >
+        {(address || isLoggedIn && email) ? email : 'Sign in'}
+      </Button>
+    ) : (
+      <Menu matchWidth>
+        {({ isOpen }) => (
+          <>
+            <MenuButton 
+              as={Button}
+              isLoading={connecting}
+              rightIcon={address ? isOpen ? <FiChevronUp /> : <FiChevronDown /> : undefined}
+              transition="all 1s linear"
+              _active={{
+                bg: "white",
+                color: "black",
+                opacity: 0.9,
+              }}
+              bg="white"
+              color="black"
+              _hover={{
+                bg: "white",
+                color: "black",
+                opacity: 0.9,
+              }}
+              {...rest}
+            >
+              {email}
+            </MenuButton>
+            <Portal>
+              <MenuList>
+                <MenuItem onClick={handleLoginOrLogout}>Sign Out</MenuItem>
+              </MenuList>
+            </Portal>
+          </>
+        )}
+      </Menu>
+    )}
+    </>
   )
 };
